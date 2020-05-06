602e9fb5bddaa718fd356fd889bf740d95bdf351
Aimee Lynn Ramirez <aimeelynnramirez@Aimees-MacBook-Pro.local>
2020-05-06 01:48:57 -0400

    deploy task

---
+++ precache-manifest.ee32e3772a96fafc295cfbb7ba674a3d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdb599dc5ca5eb8027a00fec446c6aa8",
+    "url": "/aimee-typescript-reactjs/index.html"
+  },
+  {
+    "revision": "880750122de3ffb826b3",
+    "url": "/aimee-typescript-reactjs/static/css/main.6dfb5941.chunk.css"
+  },
+  {
+    "revision": "e8a211d154cbe977d59a",
+    "url": "/aimee-typescript-reactjs/static/js/2.20cf19c5.chunk.js"
+  },
+  {
+    "revision": "880750122de3ffb826b3",
+    "url": "/aimee-typescript-reactjs/static/js/main.1d927a76.chunk.js"
+  },
+  {
+    "revision": "98e1349e9287e83695c5",
+    "url": "/aimee-typescript-reactjs/static/js/runtime-main.e422ac81.js"
+  }
+]);
